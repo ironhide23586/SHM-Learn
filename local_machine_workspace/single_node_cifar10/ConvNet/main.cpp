7208072ee27442d7f5ab0123c4b23711cfcc0330
IRONHIDE-ALIEN2\monst <sbiswas7@hawk.iit.edu>
2017-04-04 17:15:12 -0500

    cleaned code and started file-reading implementation

--- local_machine_workspace/single_node_cifar10/ConvNet/main.cpp
+++ local_machine_workspace/single_node_cifar10/ConvNet/main.cpp
@@ -108,27 +108,6 @@ float my_rand() {
   return dist(re);
 }
 
-void readBatch(FILE *fp, float *h_imgs, float *h_lbls) {
-  int row_size = (CHANNELS * DATA_SIDE * DATA_SIDE) + 1;
-  int batch_bytes = BATCH_SIZE * row_size;
-  int start_idx;
-  unsigned char *buff = (unsigned char *)malloc(sizeof(unsigned char) * BATCH_SIZE
-                                                * ((CHANNELS * DATA_SIDE * DATA_SIDE)
-                                                   + 1));
-  fread(buff, sizeof(unsigned char), batch_bytes, fp);
-  memset(h_lbls, 0, sizeof(float) * BATCH_SIZE * LABELS);
-  for (int i = 0; i < BATCH_SIZE; i++) {
-    start_idx = i * row_size;
-    h_lbls[((int)buff[start_idx] - 1) + i * LABELS] = 1.0f;
-    int col = 0;
-    for (int j = start_idx + 1; j < start_idx + row_size; j++) {
-      h_imgs[col + i * (row_size - 1)] = (float)buff[j];
-      h_imgs[col + i * (row_size - 1)] /= 255.0f;
-      col++;
-    }
-  }
-}
-
 void readBatch_mnist(FILE *fp_x, FILE *fp_y, float *h_imgs, float *h_lbls) {
   int row_size_x = (CHANNELS * DATA_SIDE * DATA_SIDE);
   int batch_bytes_x = BATCH_SIZE * row_size_x;
@@ -190,6 +169,27 @@ void readBatch_mnist_lim(FILE *fp_x, FILE *fp_y, float *h_imgs, float *h_lbls) {
   read_imgs += curr_example;
 }
 
+void readBatch_cifar10_lim(FILE *fp, float *h_imgs, float *h_lbls) {
+  int row_size = (CHANNELS * DATA_SIDE * DATA_SIDE) + 1;
+  int batch_bytes = BATCH_SIZE * row_size;
+  int start_idx;
+  unsigned char *buff = (unsigned char *)malloc(sizeof(unsigned char) * BATCH_SIZE
+                                                * ((CHANNELS * DATA_SIDE * DATA_SIDE)
+                                                   + 1));
+  fread(buff, sizeof(unsigned char), batch_bytes, fp);
+  memset(h_lbls, 0, sizeof(float) * BATCH_SIZE * LABELS);
+  for (int i = 0; i < BATCH_SIZE; i++) {
+    start_idx = i * row_size;
+    h_lbls[((int)buff[start_idx] - 1) + i * LABELS] = 1.0f;
+    int col = 0;
+    for (int j = start_idx + 1; j < start_idx + row_size; j++) {
+      h_imgs[col + i * (row_size - 1)] = (float)buff[j];
+      h_imgs[col + i * (row_size - 1)] /= 255.0f;
+      col++;
+    }
+  }
+}
+
 void move_to_gpu_stage(float *x, float *y, float *gpu_stage, int x_len, int y_len) {
   memcpy(gpu_stage, x, sizeof(float) * x_len);
   memcpy(&gpu_stage[x_len], y, sizeof(float) * y_len);
@@ -221,44 +221,29 @@ int main() {
   cublasStatus_stat = cublasCreate_v2(&cublasHandle);
   std::cout << "cublas handle create -->" << cublasStatus_stat << std::endl;
 
+  cudnnHandle_t cudnnHandle;
+  cudnnStatus_t cudnn_status;
+  cudnn_status = cudnnCreate(&cudnnHandle);
+  std::cout << "cuDNN initialization -->" << cudnn_status << std::endl;
+
   float *x, *y;
-  //x = (float *)malloc(sizeof(float) * BATCH_SIZE * CHANNELS * DATA_SIDE * DATA_SIDE);
-  //y = (float *)malloc(sizeof(float) * BATCH_SIZE * LABELS);
+
   cudaMallocHost((void **)&x, sizeof(float) * BATCH_SIZE * CHANNELS * DATA_SIDE * DATA_SIDE);
   cudaMallocHost((void **)&y, sizeof(float) * BATCH_SIZE * LABELS);
-  //float *gpu_data_stage;
-  //cudaMallocHost((void **)&gpu_data_stage, sizeof(float) * ((BATCH_SIZE * CHANNELS * DATA_SIDE * DATA_SIDE)
-  //                                                          + (BATCH_SIZE * LABELS)));
-  //
-
-  float y_dist[LABELS];
 
   float *x_test = (float *)malloc(sizeof(float) * BATCH_SIZE * CHANNELS * DATA_SIDE * DATA_SIDE);
   float *y_test = (float *)malloc(sizeof(float) * BATCH_SIZE * LABELS);
 
-  FILE *fp_x = fopen("train-images.idx3-ubyte", "rb");
-  FILE *fp_y = fopen("train-labels.idx1-ubyte", "rb");
+  FILE *fp_data_train = fopen("cifar-10-binary\cifar-10-batches-bin\data_batch_1.bin", "rb");
+  FILE *fp_data_test = fopen("cifar-10-binary\cifar-10-batches-bin\test_batch.bin", "rb");
 
-  FILE *fp_x_test = fopen("t10k-images.idx3-ubyte", "rb");
-  FILE *fp_y_test = fopen("t10k-labels.idx1-ubyte", "rb");
+  readBatch_cifar10_lim(fp_data_test, x_test, y_test);
 
   ofstream results_file;
   results_file.open("shmlearn_results.txt");
-
-  fseek(fp_x, 16, 0);
-  fseek(fp_y, 8, 0);
-
-  fseek(fp_x_test, 16, 0);
-  fseek(fp_y_test, 8, 0);
-
-  readBatch_mnist_lim(fp_x_test, fp_y_test, x_test, y_test);
+  
   read_imgs = 0;
-
-  cudnnHandle_t cudnnHandle;
-
-  cudnnStatus_t cudnn_status;
-  cudnn_status = cudnnCreate(&cudnnHandle);
-  std::cout << "cuDNN initialization -->" << cudnn_status << std::endl;
+  
 
   float base_lr = 0.05f, gamma = 0.4f, power = 0;
   float lr = base_lr * powf(1 + gamma, -power);
@@ -269,8 +254,6 @@ int main() {
                64, false, lr, mom, reg);
   fcl0.SetActivationFunc(CUDNN_ACTIVATION_SIGMOID);
   fcl0.is_input_layer = true;
-
-  //print_d_var3(fcl0.d_weight_matrix, fcl0.weight_matrix_rows, fcl0.weight_matrix_cols, false);
 
   FCLayer fcl2(cudnnHandle, cublasHandle, cudaProp, fcl0.input_batch_size,
                fcl0.output_neurons, LABELS, true, lr, mom, reg);
@@ -287,21 +270,13 @@ int main() {
   float batch_verify;
   float ts = 0.0f;
   int cnt = 1;
-  //while (batch <= lim && epoch <= EPOCHS) {
-  auto now0 = std::chrono::high_resolution_clock::now();
-  auto now1_sft = std::chrono::high_resolution_clock::now();
-  auto now1_lyr = std::chrono::high_resolution_clock::now();
-  auto now2 = std::chrono::high_resolution_clock::now();
 
   auto train_start = std::chrono::high_resolution_clock::now();
   auto train_end = std::chrono::high_resolution_clock::now();
   float dur0, dur1_sft, dur1_lyr, dur2;
 
-  //print_d_var3(fcl0.d_weight_matrix, fcl0.weight_matrix_rows, fcl0.weight_matrix_cols, false);
-  //print_d_var3(fcl2.d_weight_matrix, fcl2.weight_matrix_rows, fcl2.weight_matrix_cols);
-
   while (epoch <= EPOCHS) {
-    readBatch_mnist_lim(fp_x, fp_y, x, y);
+    readBatch_cifar10_lim(fp_data_train, x, y);
     if (prog == 1) {
       prev_read_imgs = read_imgs;
     }
@@ -309,81 +284,40 @@ int main() {
     fcl0.learning_rate = lr;
     fcl2.learning_rate = lr;
 
-    //fcl0.LoadData(x, false);
+    // Forward Pass
     train_start = std::chrono::high_resolution_clock::now();
     fcl0.LoadData(x, false);
-    now0 = std::chrono::high_resolution_clock::now();
     fcl0.ForwardProp();
     fcl2.LoadData(fcl0.d_out, true);
     fcl2.ForwardProp();
 
-    //print_d_var3(fcl0.d_data, fcl0.input_batch_size, fcl0.input_neurons);
-    //print_d_var3(fcl0.d_out, fcl0.input_batch_size, fcl0.output_neurons, false);
-    //print_d_var3(fcl2.d_out_xw_act, fcl2.input_batch_size, fcl2.output_neurons);
-    //print_d_var3(fcl2.d_out, fcl2.input_batch_size, fcl2.output_neurons);
-    //return 0;
-
-    //print_d_var3(fcl0.d_weight_matrix, fcl0.weight_matrix_rows, fcl0.weight_matrix_cols);
-    //print_d_var3(fcl0.d_data, BATCH_SIZE, fcl0.input_neurons + 1);
-
-    //print_d_var3(fcl0.d_out, BATCH_SIZE, fcl0.output_neurons + 1);
-
-    //dur0 = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(now0 - train_start).count() * 1e-9f;
-
+    // Back-propagation
     fcl2.ComputeSoftmaxGradients(y);
-    now1_sft = std::chrono::high_resolution_clock::now();
-    //dur1_sft = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(now1_sft - now0).count() * 1e-9f;
     fcl0.ComputeLayerGradients(fcl2.d_prev_layer_derivatives);
-    now1_lyr = std::chrono::high_resolution_clock::now();
-    //dur1_lyr = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(now1_lyr - now1_sft).count() * 1e-9f;
-
-    //print_d_var3(fcl2.d_gradients, fcl2.weight_matrix_rows, fcl2.weight_matrix_cols);
-
     fcl2.UpdateWeights(fcl2.d_gradients);
     fcl0.UpdateWeights(fcl0.d_gradients);
-    now2 = std::chrono::high_resolution_clock::now();
 
-    dur0 = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(now0 - train_start).count() * 1e-9f;
-    dur1_sft = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(now1_sft - now0).count() * 1e-9f;
-    dur1_lyr = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(now1_lyr - now1_sft).count() * 1e-9f;
-    dur2 = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(now2 - now1_lyr).count() * 1e-9f;
     train_end = std::chrono::high_resolution_clock::now();
-    //float dur = dur0 + dur1_sft + dur1_lyr + dur2;
 
-    //print_d_var3(fcl0.d_data, fcl0.input_batch_size, fcl0.input_neurons);
-    //return 0;
     fcl0.ForwardProp();
     fcl2.LoadData(fcl0.d_out, true);
-    //print_d_var3(fcl2.d_data, fcl2.input_batch_size, fcl2.input_neurons, false);
     fcl2.ForwardProp();
-
-    //now0 = std::chrono::high_resolution_clock::now();
     cudaMemcpy(h_out, fcl2.d_out, sizeof(float) * BATCH_SIZE * LABELS, cudaMemcpyDeviceToHost);
-    //h_out = fcl2.d_out;
-    //now2 = std::chrono::high_resolution_clock::now();
-    //dur0 = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(now2 - now0).count() * 1e-9f;
 
     my_loss = 0.0f;
     for (int i = 0; i < BATCH_SIZE; i++) {
       for (int j = 0; j < LABELS; j++) {
-        //std::cout << y[j + i * LABELS] << "," << h_out[j + i * LABELS] << "[" << j << "] ";
         my_loss -= ((y[j + i * LABELS] * log(h_out[j + i * LABELS])));
       }
-      //std::cout << std::endl;
-      //return 0;
     }
     my_loss /= BATCH_SIZE;
     wt_sum = 0.0f;
     fcl0_wt_sum = matrix_square_sum(fcl0.d_weight_matrix, fcl0.weight_matrix_size, fcl0.weight_matrix_cols);
     fcl2_wt_sum = matrix_square_sum(fcl2.d_weight_matrix, fcl2.weight_matrix_size, fcl2.weight_matrix_cols);
-    //print_d_var3(fcl0.d_weight_matrix, fcl0.weight_matrix_rows, fcl0.weight_matrix_cols, false);
-    //print_d_var3(fcl2.d_weight_matrix, fcl2.weight_matrix_rows, fcl2.weight_matrix_cols);
     wt_sum = fcl0_wt_sum + fcl2_wt_sum;
-    //print_d_var3(fcl2.d_weight_matrix, fcl2.weight_matrix_rows, fcl2.weight_matrix_cols);
     float wt_loss = (reg * 0.5f) * wt_sum;
-    //print_d_var3(fcl0.d_weight_matrix, fcl0.weight_matrix_rows, fcl0.weight_matrix_cols);
     loss = my_loss + wt_loss;
-    //print_d_var3(fcl2.d_out, BATCH_SIZE, fcl2.output_neurons);
+
     dur = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(train_end - train_start).count() * 1e-9f;
     ts += dur;
     if (cnt == 253) {
@@ -397,27 +331,17 @@ int main() {
     }
     std::cout << "Batch " << batch
       << " Epoch = " << epoch << " Loss = " << my_loss << " C++_CUDA_GPU Avg iter time = " << avg_dur;
-    //std::cout << "Batch " << batch
-    //  << " Epoch = " << epoch << " C++_CUDA_GPU Avg iter time = " << avg_dur
-    //  << ", dur0 = " << dur0 << ", dur1_sft = " << dur1_sft << ", dur1_lyr = "
-    //  << dur1_lyr << ", dur2 = " << dur2;
+
     batch++;
     prog++;
 
     if (read_imgs < prev_read_imgs) {
       batch = 1;
       epoch++;
-      fseek(fp_x, 16, 0);
-      fseek(fp_y, 8, 0);
-      //lr *= 0.98f;
-      //lr = base_lr * powf(1 + gamma * cnt, -power);
     }
     prev_read_imgs = read_imgs;
 
-    now0 = std::chrono::high_resolution_clock::now();
     results_file << ts << " " << my_loss << "\n";
-    now2 = std::chrono::high_resolution_clock::now();
-    dur0 = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(now2 - now0).count() * 1e-9f;
 
     std::cout << std::endl;
     cnt++;
@@ -431,16 +355,6 @@ int main() {
   cudnnDestroy(cudnnHandle);
   cublasDestroy_v2(cublasHandle);
 
-  //print_d_var3(fcl1.d_out, fcl1.input_batch_size, fcl1.output_neurons);
-
-  //std::cout << "Elapsed time to train " << epoch - 1
-  //  << " Epochs (1 Epoch has " << EPOCH_SIZE / BATCH_SIZE << " batches) = "
-  //  << (float) elapsed.count() / 1000000 << " s" << std::endl;
-  //std::cout << "Time per batch of " << BATCH_SIZE << " images = "
-  //  << (float)elapsed.count() / ((epoch - 1) * EPOCH_SIZE / BATCH_SIZE) / 1000000
-  //  << " s";
-
-  //print_d_var3(fcl1.d_out, fcl1.input_batch_size, fcl1.output_neurons);
   int k = getchar();
   return 0;
 }
