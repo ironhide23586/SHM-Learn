d7506cd43c397eea209981c6746f0fe71ce9bd71
IRONHIDE-ALIEN2\monst <sbiswas7@hawk.iit.edu>
2017-04-10 04:08:43 -0500

    Full L2 loss computation added to CIFAR-10 training which seems to converge on local machine; testing with K80 pending

--- local_machine_workspace/single_node_cifar10/ConvNet/main.cpp
+++ local_machine_workspace/single_node_cifar10/ConvNet/main.cpp
@@ -5,9 +5,9 @@
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 
-#include <opencv2/core/core.hpp>
-#include <opencv2/opencv.hpp>
-#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/core/core.hpp>
+//#include <opencv2/opencv.hpp>
+//#include <opencv2/highgui/highgui.hpp>
 
 #include <cudnn.h>
 
@@ -22,7 +22,7 @@
 //#include <math.h>
 
 #include <fstream>
-#include <string.h>
+#include <string>
 
 inline std::string separator() {
 #ifdef _WIN32
@@ -35,16 +35,16 @@ inline std::string separator() {
 #define DATA_SIDE 32 //Throws GPU setup error if above 257
 #define CHANNELS 3
 
-#define BATCH_SIZE 4
+#define BATCH_SIZE 128
 #define LABELS 10
 
 #define EPOCHS 10
 
-#define EPOCH_COMPONENT_SIZE 6
-#define EPOCH_SIZE 30
+#define EPOCH_COMPONENT_SIZE 10000
+#define EPOCH_SIZE 50000
 
 using namespace std;
-using namespace cv;
+//using namespace cv;
 
 int read_imgs_local, read_imgs_global;
 int data_file_idx;
@@ -101,11 +101,22 @@ void sumCols(float *mat, int rows, int cols, float *sums) {
   }
 }
 
-float matrix_square_sum(float *d_mat, int sz, int cols) {
+float matrix_square_sum_exclude_bias(float *d_mat, int sz, int cols) {
   float *tmp = (float *)malloc(sizeof(float) * sz);
   cudaMemcpy(tmp, d_mat, sizeof(float) * sz, cudaMemcpyDeviceToHost);
   float ans = 0.0;
   for (int i = cols; i < sz; i++) {
+    ans += (tmp[i] * tmp[i]);
+  }
+  free(tmp);
+  return ans;
+}
+
+float matrix_square_sum(float *d_mat, int sz) {
+  float *tmp = (float *)malloc(sizeof(float) * sz);
+  cudaMemcpy(tmp, d_mat, sizeof(float) * sz, cudaMemcpyDeviceToHost);
+  float ans = 0.0;
+  for (int i = 0; i < sz; i++) {
     ans += (tmp[i] * tmp[i]);
   }
   free(tmp);
@@ -178,7 +189,7 @@ void readBatch_cifar10_lim_v2(FILE *fp, float *h_imgs, float *h_lbls) {
     int col = 0;
     for (int j = start_idx + 1; j < start_idx + row_size; j++) {
       h_imgs[col + i * row_size_x] = (float)buff[j];
-      //h_imgs[col + i * row_size_x] /= 255.0f;
+      h_imgs[col + i * row_size_x] /= 255.0f;
       col++;
     }
   }
@@ -197,33 +208,33 @@ int my_floorf_division(float a, float b) {
   return ((a - 1) / b);
 }
 
-void show_img(cv::Mat &img) {
-  cv::Mat img_scaled = cv::Mat(600, 600, CV_8UC3);
-  cv::resize(img, img_scaled, img_scaled.size());
-  cv::namedWindow("image");
-  cv::imshow("image", img_scaled);
-  cv::waitKey();
-}
+//void show_img(cv::Mat &img) {
+//  cv::Mat img_scaled = cv::Mat(600, 600, CV_8UC3);
+//  cv::resize(img, img_scaled, img_scaled.size());
+//  cv::namedWindow("image");
+//  cv::imshow("image", img_scaled);
+//  cv::waitKey();
+//}
 
-cv::Mat lin2mat(float *img_lin) {
-  cv::Mat ans = cv::Mat(DATA_SIDE, DATA_SIDE, CV_8UC3); 
-  for (int r = 0; r < DATA_SIDE; r++) {
-    for (int c = 0; c < DATA_SIDE; c++) {
-      for (int chan = 0; chan < CHANNELS; chan++) {
-        ans.data[chan + c * CHANNELS + r * DATA_SIDE * CHANNELS] 
-          = img_lin[c + r * DATA_SIDE + chan * DATA_SIDE * DATA_SIDE];
-      }
-    }
-  }
-  return ans;
-}
-
-const char* get_train_file_path(int data_file_idx) {
-  std::string train_file = "cifar-10-binary" + separator() + "cifar-10-batches-bin" + separator()
-    + "data_batch_"
-    + std::to_string(data_file_idx) + ".bin";
-  return train_file.c_str();
-}
+//cv::Mat lin2mat(float *img_lin) {
+//  cv::Mat ans = cv::Mat(DATA_SIDE, DATA_SIDE, CV_8UC3); 
+//  for (int r = 0; r < DATA_SIDE; r++) {
+//    for (int c = 0; c < DATA_SIDE; c++) {
+//      for (int chan = 0; chan < CHANNELS; chan++) {
+//        ans.data[chan + c * CHANNELS + r * DATA_SIDE * CHANNELS] 
+//          = img_lin[c + r * DATA_SIDE + chan * DATA_SIDE * DATA_SIDE];
+//      }
+//    }
+//  }
+//  return ans;
+//}
+//
+//const char* get_train_file_path(int data_file_idx) {
+//  std::string train_file = "cifar-10-binary" + separator() + "cifar-10-batches-bin" + separator()
+//    + "data_batch_"
+//    + std::to_string(data_file_idx) + ".bin";
+//  return train_file.c_str();
+//}
 
 int main() {
   cudaError_t cudaError_stat;
@@ -246,7 +257,8 @@ int main() {
 
 
   int batch_size = BATCH_SIZE;
-  float my_loss, loss, wt_sum, cl0_wt_sum, fcl0_wt_sum, fcl2_wt_sum, dur, avg_dur;
+  float my_loss, loss, wt_sum, cl0_wt_sum, cl1_wt_sum, cl2_wt_sum, fcl0_wt_sum, fcl1_wt_sum,
+    fcl2_wt_sum, dur, avg_dur;
   cublasHandle_t cublasHandle;
   cublasStatus_stat = cublasCreate_v2(&cublasHandle);
   std::cout << "cublas handle create -->" << cublasStatus_stat << std::endl;
@@ -278,7 +290,7 @@ int main() {
   FILE *fp_data_test = fopen(test_file.c_str(), "r");  
 
   ofstream results_file;
-  results_file.open("shmlearn_results.txt");
+  /*results_file.open("shmlearn_results.txt");*/
 
   std::vector<std::string> labels =
   {
@@ -295,17 +307,17 @@ int main() {
   };
 
 
-  while (true) {
-    readBatch_cifar10_lim_v2(fp_data_train, x, y);
-    for (int i = 0; i < BATCH_SIZE; i++) {
-      for (int k = 0; k < LABELS; k++) {
-        if (y[k + i * LABELS] > 0)
-          std::cout << labels[k] << std::endl;
-      }
-      show_img(lin2mat(&x[i * 3072]));
-    }
-    std::cout << "----------" << std::endl;
-  }
+  //while (true) {
+  //  readBatch_cifar10_lim_v2(fp_data_train, x, y);
+  //  for (int i = 0; i < BATCH_SIZE; i++) {
+  //    for (int k = 0; k < LABELS; k++) {
+  //      if (y[k + i * LABELS] > 0)
+  //        std::cout << labels[k] << std::endl;
+  //    }
+  //    show_img(lin2mat(&x[i * 3072]));
+  //  }
+  //  std::cout << "----------" << std::endl;
+  //}
   
   
   read_imgs_local = 0;
@@ -467,17 +479,26 @@ int main() {
       }
     }
     my_loss /= BATCH_SIZE;
-    //wt_sum = 0.0f;
-    //
-    //fcl0_wt_sum = matrix_square_sum(fcl0.d_weight_matrix, fcl0.weight_matrix_size, 
-    //                                fcl0.weight_matrix_cols);
-    //fcl2_wt_sum = matrix_square_sum(fcl2.d_weight_matrix, fcl2.weight_matrix_size, 
-    //                                fcl2.weight_matrix_cols);
-    //
-    //wt_sum = fcl0_wt_sum + fcl2_wt_sum;
-    //float wt_loss = (reg * 0.5f) * wt_sum;
+    wt_sum = 0.0f;
     
-    loss = my_loss;// + wt_loss;
+    cl0_wt_sum = matrix_square_sum(cl0.d_filt, cl0.input_c * cl0.feature_maps
+                                   * cl0.kernel_h * cl0.kernel_w);
+    cl1_wt_sum = matrix_square_sum(cl1.d_filt, cl1.input_c * cl1.feature_maps
+                                   * cl1.kernel_h * cl1.kernel_w);
+    cl2_wt_sum = matrix_square_sum(cl2.d_filt, cl2.input_c * cl2.feature_maps
+                                   * cl2.kernel_h * cl2.kernel_w);
+
+    fcl0_wt_sum = matrix_square_sum_exclude_bias(fcl0.d_weight_matrix, fcl0.weight_matrix_size,
+                                                 fcl0.weight_matrix_cols);
+    fcl1_wt_sum = matrix_square_sum_exclude_bias(fcl1.d_weight_matrix, fcl1.weight_matrix_size,
+                                                 fcl1.weight_matrix_cols);
+    fcl2_wt_sum = matrix_square_sum_exclude_bias(fcl2.d_weight_matrix, fcl2.weight_matrix_size,
+                                                 fcl2.weight_matrix_cols);
+    
+    wt_sum = cl0_wt_sum + cl1_wt_sum + cl2_wt_sum + fcl0_wt_sum + fcl1_wt_sum + fcl2_wt_sum;
+    float wt_loss = (reg * 0.5f) * wt_sum;
+    
+    loss = my_loss + wt_loss;
     
       
     dur = (float)std::chrono::duration_cast<std::chrono::nanoseconds>(train_end 
@@ -494,7 +515,7 @@ int main() {
       avg_dur /= 2;
     }
     std::cout << "Batch " << batch
-      << " Epoch = " << epoch << " Loss = " << my_loss 
+      << " Epoch = " << epoch << " Loss = " << loss 
       << " C++_CUDA_GPU Avg iter time = " << avg_dur;
 
     batch++;
@@ -505,13 +526,14 @@ int main() {
       epoch++;
     }
     prev_read_imgs = read_imgs_global;
-
-    results_file << ts << " " << my_loss << "\n";
-
+    results_file.open("shmlearn_results.txt", std::ofstream::out | std::ofstream::app);
+    results_file << ts << " " << loss << "\n";
+    results_file.close();
     std::cout << std::endl;
     cnt++;
     
   }
+  results_file.open("shmlearn_results.txt", std::ofstream::out | std::ofstream::app);
   results_file << "Avg iter time = " << avg_dur << " seconds\n";
   results_file.close();
 
