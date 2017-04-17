45abee4177aa0b8cbe0418dbedb3fba7b8a881ed
Souham Biswas <souham@cooleylogin1.fst.alcf.anl.gov>
2017-04-17 05:26:30 +0000

    Gather & Broadcast rough code running properly :trollface:

--- cooley_workspace/rough/mpi_test/mpi_test.cpp
+++ cooley_workspace/rough/mpi_test/mpi_test.cpp
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <mpi.h>
 
-#define BCAST_SIZE 35
+#define BCAST_SIZE 6
+#define GATHER_CHUNK 3
 #define PROBED_RANK 3
 
 using namespace std;
@@ -13,7 +14,6 @@ void print_h_var3(float *h_v, int r, int c) {
   for (int i = 0; i < r; i++) {
     for (int j = 0; j < c; j++) {
       std::cout << h_v[j + i * c] << "\t";
-      //printf("%f\t", h_v_local[j + i * c]);
     }
     std::cout << std::endl;
   }
@@ -22,7 +22,9 @@ void print_h_var3(float *h_v, int r, int c) {
 int main() {
   MPI_Init(NULL, NULL);
 
-  float *data = (float *) malloc(BCAST_SIZE * sizeof(float));
+  float *data = (float *)malloc(BCAST_SIZE * sizeof(float));
+  float *gathered_data;
+  float *gather_part = (float *)malloc(GATHER_CHUNK * sizeof(float));
 
   int world_size;
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
@@ -46,20 +48,50 @@ int main() {
     }
     std::cout << "Data broadcasted -" << std::endl;
     print_h_var3(data, 1, BCAST_SIZE);
+    gathered_data = (float *)malloc(GATHER_CHUNK * world_size * sizeof(float));
   }
+
+  for (int i = my_rank * GATHER_CHUNK; i < (my_rank + 1) * GATHER_CHUNK; i++) {
+    gather_part[i - (my_rank * GATHER_CHUNK)] = i;
+  }
+
 
   MPI_Barrier(MPI_COMM_WORLD);
 
-  if (my_rank == PROBED_RANK) {
-    std::cout << "\nBefore Broadcast, Rank " << my_rank << std::endl;
-    print_h_var3(data, 1, BCAST_SIZE);
+  for (int i = 0; i < world_size; i++) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (my_rank == i) {
+      std::cout << "\nBefore Broadcast, Rank " << my_rank << std::endl;
+      print_h_var3(data, 1, BCAST_SIZE);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
   }
 
   MPI_Bcast(data, BCAST_SIZE, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-  if (my_rank == PROBED_RANK) {
-    std::cout << "\nAfter Broadcast, Rank " << my_rank << std::endl;
-    print_h_var3(data, 1, BCAST_SIZE);
+  MPI_Barrier(MPI_COMM_WORLD);
+  for (int i = 0; i < world_size; i++) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    if (my_rank == i) {
+      std::cout << "\nAfter Broadcast, Rank " << my_rank << std::endl;
+      print_h_var3(data, 1, BCAST_SIZE);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (my_rank == 0) {
+    std::cout << "\nBefore Gather -" << std::endl;
+    print_h_var3(gathered_data, 1, world_size * GATHER_CHUNK);
+  }
+
+  MPI_Gather(gather_part, GATHER_CHUNK, MPI_FLOAT, gathered_data, GATHER_CHUNK,
+             MPI_FLOAT, 0, MPI_COMM_WORLD);
+
+  MPI_Barrier(MPI_COMM_WORLD);
+  if (my_rank == 0) {
+    std::cout << "\nAfter Gather -" << std::endl;
+    print_h_var3(gathered_data, 1, world_size * GATHER_CHUNK);
   }
 
   MPI_Finalize();
